459e42bf87913047076a3c46ecfe5bf7d5567955
Meirzhan14 <meirzhan-250@mail.ru>
2019-02-18 00:17:48 +0600

    initial commit

---
+++ src/main/java/com/rest/hellorest/Greeting.java
@@ -0,0 +1,22 @@
+package com.rest.hellorest;
+
+public class Greeting {
+
+    private String name = "";
+
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public Greeting() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
